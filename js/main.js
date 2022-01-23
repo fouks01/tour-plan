a552c75bbc15e1c29378c16a76d25f6c27e349c5
acedemy-glo <tau9501@gmail.com>
2022-01-23 17:41:53 +0300

    create: added reviews section & fixed hotel slider

--- js/main.js
+++ js/main.js
@@ -12,14 +12,14 @@ const hotelSlider = new Swiper('.hotel-slider', {
     onlyInViewport: false,
   },
 });
-const swiper = new Swiper('.hotel-slider', {
+const reviewsSlider = new Swiper('.reviews-slider', {
   // Optional parameters
   loop: true,
 
   // Navigation arrows
   navigation: {
-    nextEl: '.hotel-slider__button--next',
-    prevEl: '.hotel-slider__button--prev',
+    nextEl: '.reviews-slider__button--next',
+    prevEl: '.reviews-slider__button--prev',
   },
   keyboard: {
     enabled: true,
