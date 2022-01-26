17cd17805d68994586e9f1fceb2424357b5a5980
acedemy-glo <tau9501@gmail.com>
2022-01-26 18:47:15 +0300

    create: mobile menu

--- js/main.js
+++ js/main.js
@@ -26,6 +26,14 @@ const reviewsSlider = new Swiper('.reviews-slider', {
     onlyInViewport: false,
   },
 });
+
+var menuButton = document.querySelector(".menu-button");
+menuButton.addEventListener("click", function () {
+  console.log("Клик по кнопке меню");
+  document
+    .querySelector(".navbar-bottom")
+    .classList.toggle("navbar-bottom--visible");
+});
 // var myMap;
 
 // // Дождёмся загрузки API и готовности DOM.
