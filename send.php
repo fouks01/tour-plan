84f6f1b81648a55281111da363f6635c2771ee9c
acedemy-glo <tau9501@gmail.com>
2022-02-01 02:30:57 +0300

    fixed: thankyou & php

--- send.php
+++ send.php
@@ -17,7 +17,7 @@ $body = "
 <h2>Новое обращение</h2>
 <b>Имя:</b> $name<br>
 <b>Телефон:</b> $phone<br><br>
-<b>Сообщение:</b><br>$message
+<b>Сообщение:</b>$message<br>
 <b>Новости:</b> $newsletter<br>
 ";
 
