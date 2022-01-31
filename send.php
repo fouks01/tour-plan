bce5c3ca470fcaece6d2dd9d586471d7fa60d6ec
acedemy-glo <tau9501@gmail.com>
2022-02-01 01:53:13 +0300

    create: created tel num, fixed adaptive screens

---
+++ send.php
@@ -0,0 +1,66 @@
+<?php
+// Файлы phpmailer
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+require 'phpmailer/Exception.php';
+
+// Переменные, которые отправляет пользователь
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+$newsletter = $_POST['newsletter'];
+
+
+// Формирование самого письма
+$title = "Новое обращение Best Tour Plan";
+$body = "
+<h2>Новое обращение</h2>
+<b>Имя:</b> $name<br>
+<b>Телефон:</b> $phone<br><br>
+<b>Сообщение:</b><br>$message
+<b>Новости:</b> $newsletter<br>
+";
+
+// Формирование самого письма
+$title = "Новый запрос Best Tour Plan";
+$body = "
+<h2>Новый запрос</h2>
+<b>Подписка без онлайн регистрации и СМС:</b> $newsletter<br>
+";
+
+// Настройки PHPMailer
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+try {
+    $mail->isSMTP();   
+    $mail->CharSet = "UTF-8";
+    $mail->SMTPAuth   = true;
+    // $mail->SMTPDebug = 2;
+    $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
+
+    // Настройки вашей почты
+    $mail->Host       = 'smtp.gmail.com'; // SMTP сервера вашей почты
+    $mail->Username   = 'aivengocorp@gmail.com'; // Логин на почте
+    $mail->Password   = 'YR5-2Et-yvP-fQ6'; // Пароль на почте
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port       = 465;
+    $mail->setFrom('aivengocorp@gmail.com', 'Александр Айвенго'); // Адрес самой почты и имя отправителя
+
+    // Получатель письма
+    $mail->addAddress('aivengoalek@yandex.ru');  
+
+    // Отправка сообщения
+    $mail->isHTML(true);
+    $mail->Subject = $title;
+    $mail->Body = $body;    
+
+// Проверяем отравленность сообщения
+if ($mail->send()) {$result = "success";} 
+else {$result = "error";}
+
+} catch (Exception $e) {
+    $result = "error";
+    $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+}
+
+// Отображение результата
+header('Location: thankyou.html');
